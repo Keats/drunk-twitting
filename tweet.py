e37959ef749f32500a3ffd4373c4cdec420526cd
Keats <balthek@gmail.com>
2014-10-02 20:44:04 +0100

    Allow to search for non-drunk tweets

--- tweet.py
+++ tweet.py
@@ -1,5 +1,6 @@
+import random
 from TwitterAPI import TwitterAPI
-import pprint
+
 consumer_key = "J3lMNAgfjFyqcCOjrmEDZCzsw"
 consumer_secret = "hNx7JzA1CI4fyOysTHJl8cdJDUtQWiOuVT0qlfkCL2IHO7aS72"
 access_token_key = "2360201306-dfNbwC3J4NmJBzTCsJeYkA9PUxSRer4KZRbmIjN"
@@ -7,16 +8,19 @@ access_token_secret = "T6GL0dS0hrSOQ3PqD83ezxpASNDa2nax7oykynnuflsTi"
 
 api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
+drunk_hashtags = ['#drunk', '#drinking', '#alcohol', '#beer', '#wine', '#vodka', '#tequila']
+normal_hashtags = ['#python', '#london', '#australia', '#europe', '#sport', '#startups']
 
 
-def get_tweet(number=24, search="#drunk"):
-    r = api.request('search/tweets', {'q': search, 'result_type': 'recent'})
+def get_tweet(number=24, search_for_drunks=True):
+    api_options = {
+        'result_type': 'recent',
+        'count': 200,
+        'q': random.choice(drunk_hashtags) if search_for_drunks else random.choice(normal_hashtags)
+    }
+
+    if search_for_drunks:
+        r = api.request('search/tweets', api_options)
+    else:
+        r = api.request('search/tweets', api_options)
     return [item['text'].encode('utf-8') for item in list(r.get_iterator())][:number]
-
-
-
-search_terms = ['#drunk', '#drinking', '#alcohol', '#beer', '#wine', '#vodka', '#tequila']
-for term in search_terms:
-    print term
-    for twit in get_tweet(number=10, search=term):
-        print twit
